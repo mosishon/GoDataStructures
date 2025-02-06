8c2d99dc742d3c4128c60d9683621ada994679cd
mosTafa Arshadi <mostafa1382ma@gmail.com>
2025-02-06 19:13:12 +0330

    Delete tests from main.go Add a Map to skip some tests

--- main.go
+++ main.go
@@ -3,65 +3,23 @@ package main
 import (
 	"fmt"
 	"godatastructures/linkedlist"
-	"math/rand"
-	"time"
+	"godatastructures/tree"
 )
 
 func main() {
-	var SingleLinkedList = linkedlist.SingleLinkedList[int]{}
-	var DoubleLinkedList = linkedlist.DoubleLinkedList[int]{}
-	appendCount := 1_000_000
-	getCount := 5_000
-	start := time.Now()
-	for counter := 0; counter < appendCount; counter++ {
-		SingleLinkedList.Append(counter)
+	tests := map[string]bool{
+		"test_linkedlist": false,
+		"test_tree":       true,
 	}
-	fmt.Printf("SingleLinkedList Append %d item took %s\n", appendCount, time.Since(start))
 
-	start = time.Now()
-	for counter := 0; counter < appendCount; counter++ {
-		DoubleLinkedList.Append(counter)
+	if tests["test_linkedlist"] {
+		linkedlist.LinkedListTest()
+	} else {
+		fmt.Println("[SKIP] LinkedList test")
 	}
-	fmt.Printf("DoubleLinkedList Append %d item took %s\n", appendCount, time.Since(start))
-	start = time.Now()
-	for counter := 0; counter < getCount; counter++ {
-		randomIndex := rand.Intn(appendCount)
-		SingleLinkedList.ItemAt(randomIndex)
+	if tests["test_tree"] {
+		tree.BinarySearchTreeTest()
+	} else {
+		fmt.Println("[SKIP] BinarySearchTree test")
 	}
-	fmt.Printf("SingleLinkedList Get %d random item took %s\n", getCount, time.Since(start))
-
-	start = time.Now()
-	for counter := 0; counter < getCount; counter++ {
-		randomIndex := rand.Intn(appendCount)
-		DoubleLinkedList.ItemAt(uint(randomIndex))
-	}
-	fmt.Printf("DoubleLinkedList Get %d random item took %s\n", getCount, time.Since(start))
-
-	// DoubleLinkedList Get will be faster than SingleLinkedList Get because of the way we implemented ItemAt method
-	// DoubleLinkedList can search from the end of the list if the index is closer to the end
-	// If total size grows, the difference will be more significant
-	// because SingleLinkedList will have to search from the beginning every time
-	// and the gap between the index and the end of the list will be bigger
-
-	// now we will check memory usage
-	// each single linked list has a head and a tail pointer with size 8 bytes each = 16 bytes
-	// each single node has a data with size X and a next pointer with size 8 bytes = X + 8 bytes
-
-	// each double linked list has a head, a tail pointer and a size with size 8 bytes each = 24 bytes
-	// each double linked node has a data with size X and a next, prev pointer with size 8 bytes each = X + 16 bytes
-
-	// for 100_000_000 items of int64 type (8 bytes) we will have
-	// SingleLinkedList: 16 + 1_000_000 * (8 + 8) = 16 + 16_000_000 = 16_000_016 bytes = 16 MB
-	// DoubleLinkedList: 24 + 1_000_000 * (8 + 16) = 24 + 24_000_000 = 24_000_024 bytes = 24 MB
-	// 24/16 = 1.5 times more memory usage for DoubleLinkedList
-
-	//get 5000 random item in list with 1_000_000 item took:
-	// 3.35 sec for SingleLinkedList
-	// 2.29 sec for DoubleLinkedList
-	// 3.35/2.29 = 1.46 times faster for DoubleLinkedList
-	// Note : if we increase the number of items in the list, the difference will change
-	// and the DoubleLinkedList will be faster more than 1.46 times
-	// but the memory usage will be exactly 1.5 times more than SingleLinkedList
-	// so for a large number of items, we can use DoubleLinkedList if we need to get items faster
-	// but for a small number of items, we can use SingleLinkedList if we need to save memory
 }
